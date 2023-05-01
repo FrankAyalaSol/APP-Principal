2fed291a9c7fbfa33b3729b73f88693db117438b
FrankAyalaSol <201810595@urp.edu.pe>
2023-05-01 02:40:24 -0500

    V1.5

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import {
-  View,
-  StyleSheet,
-  Dimensions
-} from 'react-native';
-import MapView from 'react-native-maps';
+import * as React from 'react';
+import {View,StyleSheet,Dimensions} from 'react-native';
+import MapView, {Marker,Polyline} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+
+
 const {width,height} = Dimensions.get('screen')
 const MapScreen = () => {
-
+const [origin,SetOrigin] = React.useState({
+    latitude:-12.130561,
+    longitude:-76.979959
+});
+const [destination,SetDestination] = React.useState({
+    latitude:-12.087880,
+    longitude:-77.034050
+});
     return(
         <View style={styles.container}>
             <MapView
             style={{width:width,height:height}}
+            initialRegion={{
+                latitude:origin.latitude,
+                longitude:origin.longitude,
+                latitudeDelta:0.09,
+                longitudeDelta:0.04
+            }}
+            >
+                <Marker
+                    draggable
+                    coordinate={origin}
+                    onDragEnd={(direction) => SetOrigin(direction.nativeEvent.coordinate)}
+                />
+                <Marker
+                    draggable
+                    coordinate={destination}
+                    onDragEnd={(direction) => SetDestination(direction.nativeEvent.coordinate)}
+                />
+                <MapViewDirections
+                origin={origin}
+                destination={destination}
+                apiKey={'AIzaSyBQ4GAn3F0m_WTrA93SHaT1xJYHZJfwDNE'}
+                />
+                <Polyline
+                coordinates={[origin,destination]}
+                strokeColor='blue'
+                strokeWidth={8}
+                />
+            </MapView>
             
-            />
+
         </View>
     );
 }
